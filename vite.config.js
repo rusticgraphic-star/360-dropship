0bedc84e2ff99bf2d4247a0e31a975a986eafd1a
rusticgraphic-star <rustic.graphic@gmail.com>
2026-07-27 19:07:19 +0530

    Sync vite.config.js for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-[hash]-${Date.now()}[extname]`
+      }
+    }
+  }
 })
